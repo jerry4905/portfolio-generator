ca3cbadaab686a77128e1dc793e0c12a2e5b77d6
jerry <jerry4905@hotmail.com>
2021-06-09 14:55:22 -0700

    modulize

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
+const [name1, github] = profileDataArgs;
 
+
+//console.log(profileDataArgs);
+//const printProfileData = profileDataArr => {
     // this
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('================');
-
+//    for (let i = 0; i < profileDataArr.length; i++) {
+//        console.log(profileDataArr[i]);
+//    } console.log('================');
     //is the same as this
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+    //profileDataArr.forEach((profileItem) => console.log(profileItem));};
+//printProfileData(profileDataArgs);
 
-printProfileData(profileDataArgs);
+  fs.writeFile('index.html', generatePage(name1, github), err => {
+    if (err) throw new Error(err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
