70899fdaaf4371d0fa9fe8daf7a0431de443a560
jerry <jerry4905@hotmail.com>
2021-06-09 16:03:30 -0700

    name capture

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name1, github] = profileDataArgs;
-
-
-//console.log(profileDataArgs);
-//const printProfileData = profileDataArr => {
-    // this
-//    for (let i = 0; i < profileDataArr.length; i++) {
-//        console.log(profileDataArr[i]);
-//    } console.log('================');
-    //is the same as this
-    //profileDataArr.forEach((profileItem) => console.log(profileItem));};
-//printProfileData(profileDataArgs);
-
-  fs.writeFile('index.html', generatePage(name1, github), err => {
-    if (err) throw new Error(err);
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
+//
+//const pageHTML = generatePage(name1, github);
+//
+//  fs.writeFile('./index.html', pageHTML, err => {
+//    if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//    console.log('Portfolio complete! Check out index.html to see the output!');
+//  });
